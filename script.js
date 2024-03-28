16fc7ed2cfc3159a6d9766f57890370f499b53c5
brodybrand <80013712+brodybrand@users.noreply.github.com>
2024-03-28 14:53:56 -0700

    refactor: add loop for button options.
    Add an array and loop to handle game options via
    button clicking.

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
-// initialize variables to default values
+// initialize score and choice variable
 let userWins = 0;
-let userLoses = 0;
+let userLosses = 0;
 let ties = 0;
 let totalGames = 0;
 let playerChoice = ''
 let computerChoice = ''
 
-const rockBtn = document.querySelector("#rock-btn");
-rockBtn.addEventListener("click", () => {
-    playerChoice= "rock";
-    computerChoice = getComputerChoice();
-    playRound(playerChoice, computerChoice);
-    updateDisplays();
-    checkTotal();
-});
+// initialize displays
+const userDisplay = document.querySelector("#user-choice")
+const cpuDisplay = document.querySelector("#computer-choice");
+const winsDisplay = document.querySelector("#wins");
+const lossesDisplay = document.querySelector("#losses");
+const tiesDisplay = document.querySelector("#ties");
 
-const paperBtn = document.querySelector("#paper-btn");
-paperBtn.addEventListener("click", () => {
-    playerChoice = "paper";
-    computerChoice = getComputerChoice();
-    playRound(playerChoice, computerChoice);    
-    updateDisplays();
-    checkTotal();
-});
+// initialize game option buttons
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
 
-const scissorsBtn = document.querySelector("#scissors-btn");
-scissorsBtn.addEventListener("click", () => {
-    playerChoice = "scissors";
-    computerChoice = getComputerChoice();
-    playRound(playerChoice, computerChoice); 
-    updateDisplays();
-    checkTotal();
-});
+// Loop through result of button clicks (play game)
+buttons = [rockBtn, paperBtn, scissorsBtn]
+buttons.forEach( (button) => {
+    button.addEventListener("click", () => {
+        playerChoice = button.id;
+        computerChoice = getComputerChoice();
+        playRound(playerChoice, computerChoice);
+        updateDisplays();
+        checkTotal();
+    });
+})
 
+// revert variables and display back to 0 or empty
 const restartButton = document.querySelector("#restart-btn");
 restartButton.addEventListener("click", () => {
     userWins = 0;
-    userLoses = 0;
+    userLosses = 0;
     ties = 0;
     totalGames = 0;
     userChoice = '';
@@ -47,49 +45,16 @@ restartButton.addEventListener("click", () => {
     updateDisplays();
 });
 
-function checkTotal() {
-    let result = document.querySelector("#game-result");
-    if (totalGames === 5) {
-        if (userWins > userLoses){
-            result.textContent = "You won!"
-        } if (userWins < userLoses){
-            result.textContent = "You lost."
-        } if (userWins === userLoses){
-            result.textContent = "Its a tie."
-        }
-    } if (totalGames > 5) {
-        result.textContent = "∞ To Infinity ∞"
-    }
-
-}
-
-function updateDisplays() {
-    userDisplay = document.querySelector("#user-choice")
-    userDisplay.textContent = ("You: " + playerChoice)
-
-    cpuDisplay = document.querySelector("#computer-choice");
-    cpuDisplay.textContent = ("Computer: " + computerChoice);
-
-    winsDisplay = document.querySelector("#wins");
-    winsDisplay.textContent = ("Wins: " + userWins);
-
-    losesDisplay = document.querySelector("#loses");
-    losesDisplay.textContent = ("Loses: " + userLoses);
-
-    tiesDisplay = document.querySelector("#ties");
-    tiesDisplay.textContent = ("Ties: " + ties); 
+function getComputerChoice() {
+    let options = ['rock', 'paper', 'scissors'];
+    choice = options[getRandomInt(0, 2)];
+    return computerChoice = choice;
 }
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getComputerChoice() {
-    let options = ['rock', 'paper', 'scissors'];
-    choice = options[getRandomInt(0, 2)];
-    return computerChoice = choice;
 }
 
 function playRound(playerChoice,computerChoice) {
@@ -99,10 +64,34 @@ function playRound(playerChoice,computerChoice) {
     } else if (playerChoice == 'rock' && computerChoice == 'paper'
         || playerChoice === 'paper' && computerChoice == 'scissors'
         || playerChoice == 'scissors' && computerChoice == 'rock') {
-            userLoses += 1;
+            userLosses += 1;
             totalGames += 1;
     } else {
         userWins += 1;
         totalGames += 1;
     }
+}
+
+function updateDisplays() {
+    userDisplay.textContent = ("You: " + playerChoice)
+    cpuDisplay.textContent = ("Computer: " + computerChoice);
+    winsDisplay.textContent = ("Wins: " + userWins);
+    lossesDisplay.textContent = ("Losses: " + userLosses);
+    tiesDisplay.textContent = ("Ties: " + ties); 
+}
+
+function checkTotal() {
+    let result = document.querySelector("#game-result");
+    if (totalGames === 5) {
+        if (userWins > userLosses){
+            result.textContent = "You won!"
+        } if (userWins < userLosses){
+            result.textContent = "You lost."
+        } if (userWins === userLosses){
+            result.textContent = "Its a tie."
+        }
+    } if (totalGames > 5) {
+        result.textContent = "∞ To Infinity ∞"
+    }
+
 }
